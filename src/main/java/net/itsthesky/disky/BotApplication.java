9abce25931f6b165748fe2fe437f36b965e8b127
Fusezion <fusezionstream@gmail.com>
2025-09-25 19:17:27 -0400

    BotApplication - Convert into record

--- src/main/java/net/itsthesky/disky/BotApplication.java
+++ src/main/java/net/itsthesky/disky/BotApplication.java
@@ -5,31 +5,9 @@ import net.itsthesky.disky.core.Bot;
 /**
  * Class that will store and handle information about a bot's application.
  */
-public class BotApplication {
-
-    private final String name;
-    private final String clientID;
-    private final String clientSecret;
-
-    public BotApplication(String name, String clientID, String clientSecret) {
-        this.name = name;
-        this.clientID = clientID;
-        this.clientSecret = clientSecret;
-    }
-
-    public String getName() {
-        return name;
-    }
+public record BotApplication(String name, String clientID, String clientSecret) {
 
     public Bot getBot() {
-        return DiSky.getManager().fromName(getName());
-    }
-
-    public String getClientID() {
-        return clientID;
-    }
-
-    public String getClientSecret() {
-        return clientSecret;
+        return DiSky.getManager().fromName(name());
     }
 }
